b186e35ff857e19f1faac5f7a49b1cb856afb76f
HadiAliHussein <144002165+HadiAliHussein@users.noreply.github.com>
2025-03-11 08:43:24 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/HadiAliHussein" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        {/* <a href="https://github.com/HadiAliHussein" target="_blank" rel="noreferrer"><GitHubIcon/></a> */}
         <a href="https://www.linkedin.com/in/hadi-ali-hussein-98837a217" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>E-Mail: <a href="mailto:hadi@alihussein.de" target="_blank" rel="noreferrer">hadi@alihussein.de</a></p>
